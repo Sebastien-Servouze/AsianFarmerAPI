6aa120a3555c6cc6e3e5bf97403ddf71cc147181
Sébastien Servouze <servouze.1803@apside-groupe.com>
2019-04-04 17:39:08 +0200

    Ajout du parse controller

--- AsianFarmerAPI/Controllers/ParseController.cs
+++ AsianFarmerAPI/Controllers/ParseController.cs
@@ -1,4 +1,5 @@
-﻿using HtmlAgilityPack;
+﻿using AsianFarmerAPI.Models;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +13,31 @@ namespace AsianFarmerAPI.Controllers
     {
         private const string URL = "https://conanexiles.gamepedia.com/";
 
-        [Route("{ingredientName:string}")]
+        [HttpGet]
+        [Route("{ingredientName}")]
         public void Parse(string ingredientName)
         {
             // Récupération du document de la page @url
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(URL + ingredientName);
 
+            // Récupération de l'image
+            string imgUrl = doc.DocumentNode.SelectNodes("//body/div/div/div/div/div/div/table/tbody")[2].SelectSingleNode("//td/a/img").Attributes[1].Value;
+
+            // Récupération des recettes possible pour créer l'ingrédient
+            HtmlNodeCollection recipesNodes = doc.DocumentNode.SelectNodes("//body/div/div/div/div/div/div/table")[2].ChildNodes[3].ChildNodes;
+            recipesNodes.Remove(0); 
+            recipesNodes.Remove(0);
+            recipesNodes.Remove(0);
+            recipesNodes.Remove(0); 
+
+            List<Recipe> recipes = new List<Recipe>();
+            Recipe tmp;
+            foreach (HtmlNode node in recipesNodes)
+            {
+                tmp = new Recipe();
+                tmp.Lines = new List<RecipeLine>();
+            }
 
         }
     }
