328e495e3a38e94d35b9ba4fa9bcccb6b2223d1f
Sébastien Servouze <servouze.1803@apside-groupe.com>
2019-04-04 16:29:46 +0200

    Ajout des packages

---
+++ AsianFarmerAPI/Controllers/ParseController.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AsianFarmerAPI.Controllers
+{
+    public class ParseController : ApiController
+    {
+        private const string URL = "https://conanexiles.gamepedia.com/";
+
+        [Route("{ingredientName:string}")]
+        public void Parse(string ingredientName)
+        {
+            // Récupération du document de la page @url
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(URL + ingredientName);
+
+
+        }
+    }
+}
